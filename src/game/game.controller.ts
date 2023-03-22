363d7ecfed746705e87dbe911e373ef57b11dd46
Mateusz <xmaten13@gmail.com>
2023-03-22 08:36:20 +0100

    feat: add methods to join and start game

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { GameService } from './game.service';
 import { CreateGameDto } from './dtos/createGame.dto';
+import { JoinGameDto } from './dtos/joinGame.dto';
 
 @Controller('game')
 export class GameController {
@@ -8,5 +9,15 @@ export class GameController {
   @Post('/create')
   async createGame(@Body() createGameDro: CreateGameDto) {
     return this.gameService.createGame(createGameDro);
+  }
+
+  @Post('/join')
+  async joinGame(@Body() joinGameDto: JoinGameDto) {
+    return this.gameService.joinGame(joinGameDto);
+  }
+
+  @Post('/:gameId/start')
+  async startGame(@Param('gameId') gameId: string) {
+    console.log('starting ', gameId);
   }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,9 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateGameDto } from './dtos/createGame.dto';
 import { Repositories } from '../constants';
 import { Repository } from 'typeorm';
 import { Game } from './game.entity';
+import { JoinGameDto } from './dtos/joinGame.dto';
 
 @Injectable()
 export class GameService {
@@ -19,6 +20,20 @@ export class GameService {
     };
 
     const game = this.gameRepository.create(gamePayload);
+
+    return this.gameRepository.save(game);
+  }
+
+  async joinGame(joinGameDto: JoinGameDto) {
+    const game = await this.gameRepository.findOne({
+      where: { gameId: joinGameDto.gameId },
+    });
+
+    if (!game) {
+      throw new NotFoundException('Game not found');
+    }
+
+    game.playerTwo = joinGameDto.username;
 
     return this.gameRepository.save(game);
   }
