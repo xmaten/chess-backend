14b8ca242cf853e254fd2bcfe5b9248f5d0c0819
Mateusz <xmaten13@gmail.com>
2023-04-11 14:00:56 +0200

    fix: add missing import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { userProviders } from './user.providers';
+import { DatabaseModule } from '../database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [...userProviders, UsersService],
   exports: [UsersService],
 })
